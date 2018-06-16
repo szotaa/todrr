c7700ed74588b311fd8274187d06266f92a08690
szotaa <kubaszota97@gmail.com>
2018-06-16 16:35:40 +0200

    initial commit v2

---
+++ todrr-backend/src/main/java/pl/szotaa/todrr/TodrrApplication.java
@@ -0,0 +1,12 @@
+package pl.szotaa.todrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodrrApplication.class, args);
+	}
+}
