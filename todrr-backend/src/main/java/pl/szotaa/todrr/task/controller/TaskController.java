4db9c3bc39e07cc63a41cb3ca14e7d10dfd18878
szotaa <kubaszota97@gmail.com>
2018-06-18 11:34:43 +0200

    add Task rest controller

---
+++ todrr-backend/src/main/java/pl/szotaa/todrr/task/controller/TaskController.java
@@ -0,0 +1,55 @@
+package pl.szotaa.todrr.task.controller;
+
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.szotaa.todrr.task.model.Task;
+import pl.szotaa.todrr.task.service.TaskService;
+
+/**
+ * Rest controller resolving various Task related requests.
+ *
+ * @author szotaa
+ */
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/task")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Void> add(@RequestBody Task task){
+        taskService.save(task);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(task.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable long id){
+        return ResponseEntity.ok(taskService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable long id, @RequestBody Task task){
+        task.setId(id);
+        taskService.update(id, task);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        taskService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
