1095341e9c121c7b8646f123a0a710d01ada0c47
szotaa <kubaszota97@gmail.com>
2018-06-19 15:51:55 +0200

    initial user repository

---
+++ todrr-backend/src/main/java/pl/szotaa/todrr/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package pl.szotaa.todrr.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.szotaa.todrr.user.model.User;
+
+/**
+ * Spring's Data JPA repository providing User's persistence functionality.
+ *
+ * @author szotaa
+ */
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
