7deb1b1015087fb81c2d3a4a32216952ec3e2aff
szotaa <kubaszota97@gmail.com>
2018-06-20 09:54:45 +0200

    add custom UserDetailsService

---
+++ todrr-backend/src/main/java/pl/szotaa/todrr/user/service/UserService.java
@@ -0,0 +1,20 @@
+package pl.szotaa.todrr.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.szotaa.todrr.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).<UsernameNotFoundException>orElseThrow(() -> {throw new UsernameNotFoundException(username);});
+    }
+}
