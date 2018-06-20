8d6158b18d4be45027805df95507267c282a2831
szotaa <kubaszota97@gmail.com>
2018-06-20 11:39:20 +0200

    initial security configration

---
+++ todrr-backend/src/main/java/pl/szotaa/todrr/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package pl.szotaa.todrr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/user").permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/task/**").authenticated();
+    }
+
+    @Configuration
+    static class SecurityBeans {
+
+        @Bean
+        public PasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder(10);
+        }
+    }
+}
