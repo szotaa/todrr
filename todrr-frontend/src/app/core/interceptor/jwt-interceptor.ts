0666550eb0b4d25f2700cd635f403f4e7807a398
szotaa <kubaszota97@gmail.com>
2018-06-27 17:46:26 +0200

    add jwt interceptor which appends jwt token to every sent request

---
+++ todrr-frontend/src/app/core/interceptor/jwt-interceptor.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs/internal/Observable";
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor{
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const jwtToken = localStorage.getItem("jwtToken");
+
+    if(jwtToken){
+      const cloned = req.clone({
+        headers: req.headers.set("Authorization", "Bearer " + jwtToken)
+      });
+
+      return next.handle(cloned);
+    }
+    return next.handle(req);
+  }
+}
