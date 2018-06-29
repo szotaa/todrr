a1727c38f601e721383b6f395da5d1a870055a66
szotaa <kubaszota97@gmail.com>
2018-06-29 17:07:57 +0200

    add proper navigation

--- todrr-frontend/src/app/common/header/header.component.ts
+++ todrr-frontend/src/app/common/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+import {Breadcrumb} from "../../core/model/breadcrumb.model";
+import {AuthService} from "../../core/service/auth.service";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  private breadcrumbs: Breadcrumb[] = [];
+
+  constructor(
+    @Inject(DOCUMENT) private document: any,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.breadcrumbs.push({path: "/", name: "Home"});
+    this.breadcrumbs.push({path: location.pathname, name: this.formatPath(location.pathname)});
   }
 
+  private formatPath(path: string): string {
+    path = path.substring(1);
+    path = path.charAt(0).toUpperCase() + path.slice(1);
+    return path;
+  }
 }
